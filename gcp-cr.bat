6223dcafba23591d6623d4ac23aca9b33cf3606b
Pedro Pablo Pastene <pe.pastene@gmail.com>
2021-05-30 22:55:31 -0400

    Arreglo en llamado de variables

--- gcp-cr.bat
+++ gcp-cr.bat
@@ -1,4 +1,4 @@
-docker build -t gcr.io/$1/realworld-backend ./Conduit_NodeJS
-docker push gcr.io/$1/realworld-backend
-docker build --target production-stage -t gcr.io/$1/realworld-frontend ./vue3-realworld-example-app
-docker push gcr.io/$1/realworld-frontend
+docker build -t gcr.io/%1/realworld-backend ./Conduit_NodeJS
+docker push gcr.io/%1/realworld-backend
+docker build --target production-stage -t gcr.io/%1/realworld-frontend ./vue3-realworld-example-app
+docker push gcr.io/%1/realworld-frontend
